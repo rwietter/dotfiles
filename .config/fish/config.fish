17af607d954456631dea9f344430b7a233c1fe15
rwietter <rwietter@zohomail.com>
2024-01-25 22:19:36 -0300

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,19 @@
 # -- configs
 # ----------
-set fish_greeting # disable fish greeting
+set -g fish_greeting # disable fish greeting
 set -g fish_history 10000 # Increase history size
-set fish_complete_select_descriptions yes
-set fish_complete_select_colorize yes
+set -g fish_complete_select_descriptions yes # Show descriptions for completions
+set -g fish_complete_select_colorize yes # Colorize completions
+set -g fish_autosuggestion_enabled 1 # Enable autosuggestions
 
+fish_config theme choose "Dracula Official" # Set theme
 
-# -- Set cursor to block and blink
-set -g fish_vi_force_cursor 1
-set fish_cursor_default line blink
-set fish_cursor_insert line blink
-set fish_cursor_replace_one line blink
-set fish_cursor_external line blink
-set fish_cursor_visual line blink
+set -g fish_vi_force_cursor 1 # Force cursor to be line
+set fish_cursor_default line blink # Set cursor to line and blink
+set fish_cursor_insert line blink # Set cursor to line and blink in insert mode
+set fish_cursor_replace_one line blink # Set cursor to line and blink in replace mode
+set fish_cursor_external line blink # Set cursor to line and blink in external mode
+set fish_cursor_visual line blink # Set cursor to line and blink in visual mode
 set -x fish_key_bindings fish_vi_key_bindings # This enables vi keybindings
 
 # -- Prompt settings
@@ -20,17 +21,17 @@ set -x fish_key_bindings fish_vi_key_bindings # This enables vi keybindings
 
 # -- Paths
 # --------
-fish_add_path -U ~/.npm-global/bin
-fish_add_path -U ~/.cargo/bin
-fish_add_path -U /home/rwietter/.spicetify
-fish_add_path -U ~/Downloads/roc_nightly-linux_x86_64-2024-01-20-ebfcd71
+fish_add_path -U ~/.npm-global/bin # Add npm global bin to path
+fish_add_path -U ~/.cargo/bin # Add cargo bin to path
+fish_add_path -U ~/.spicetify # Add spicetify to path
+fish_add_path -U ~/Downloads/roc_nightly-linux_x86_64-2024-01-20-ebfcd71 # Add roc to path
 
 # -- Environments
 # ---------------
-set -gx TERM wezterm
-set -gx EDITOR nvim
-set -gx VISUAL nvim
-set -gx BROWSER firefox
+set -gx TERM wezterm # Set terminal to wezterm
+set -gx EDITOR nvim # Set editor to nvim
+set -gx VISUAL nvim # Set visual editor to nvim
+set -gx BROWSER firefox # Set browser to firefox
 
 # -- Functions
 # ------------
