989dd1e942f3e92183a3ae2ca364c270bb7d4624
rwietter <rwietter@zohomail.com>
2024-02-05 12:13:39 -0300

    add git log follow alias

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,3 +1,7 @@
-function gi --description 'alias gi git init'
+function gin --description 'Initialize git repository'
   git init $argv
+end
+
+function glf --description 'Log file following changes <file>'
+  git log --pretty=format:'%C(yellow)%h %C(blue)%d %C(white)%s %C(cyan)%cn, %C(green)%cr' --graph --date=short --follow $argv
 end
